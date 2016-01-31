5de6b31ea56906d123a924356d33e0896bad5e59
jordan tsolov <jordan_tsolov@abv.bg>
2016-01-31 10:23:21 +0200

    OTHT= Renamed to dismember created a method for  bulls and cows

--- src/ProjectBNC.java
+++ src/ProjectBNC.java
@@ -2,13 +2,37 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class ProjectBNC {
-	public static void OTHT(int value, int[] array) {
+	public static void dismember(int value, int[] array) {
 		array[0] = value / 1000;
 		array[1] = value / 100 % 10;
 		array[2] = value / 10 % 10;
 		array[3] = value % 10;
-		// Ones, Tens , Hundreds, Thousands
 
+	}
+
+	public static void bullsAndCows(int array[], int array2[]) {
+		int bulls = 0;
+		int cows = 0;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array2.length; j++) {
+				if (i == j) {
+					if (array[i] == array2[j]) {
+						bulls++;
+					}
+					continue;
+				} else {
+					if (array[i] != array2[i]) {
+
+						if (array[i] == array2[j]) {
+
+							cows++;
+						}
+					}
+				}
+			}
+		}
+		System.out.println("You got " + cows + " cows");
+		System.out.println("You got " + bulls + " bulls");
 	}
 
 	public static boolean equalNumber(int array[]) {
@@ -35,7 +59,7 @@ public class ProjectBNC {
 		int theNumber[] = new int[4];
 		int ourGuess[] = new int[4];
 		int value = 9878;
-		OTHT(value, theNumber);
+		dismember(value, theNumber);
 		int attempts = 0;
 		int flag = 0;
 		do {
@@ -45,48 +69,28 @@ public class ProjectBNC {
 
 			} else {
 				value = rand.nextInt(9000) + 1000;
-				OTHT(value, theNumber);
+				dismember(value, theNumber);
 
 			}
 		} while (true);
 		System.out.print("Type your guess: ");
 		int guess = input.nextInt();
-		OTHT(guess, ourGuess);
+		dismember(guess, ourGuess);
 
 		while (guess != value) {
 			if (!equalNumber(ourGuess) && guess != value || guess < 1000 || guess > 9999) {
 
 				System.out.print("Please enter a correct number: ");
 				guess = input.nextInt();
-				OTHT(guess, ourGuess);
+				dismember(guess, ourGuess);
 
 			} else {
-				for (int i = 0; i < ourGuess.length; i++) {
-					for (int j = 0; j < theNumber.length; j++) {
-						if (i == j) {
-							if (ourGuess[i] == theNumber[j]) {
-								bulls++;
-							}
-							continue;
-						} else {
-							if (ourGuess[i] != theNumber[i]) {
-
-								if (ourGuess[i] == theNumber[j]) {
-
-									cows++;
-								}
-							}
-						}
-					}
-				}
-
-				System.out.println("You got " + cows + " cows");
-				System.out.println("You got " + bulls + " bulls");
+				bullsAndCows(ourGuess, theNumber);
 				System.out.print("Type your guess: ");
 				bulls = 0;
 				cows = 0;
 				guess = input.nextInt();
-				OTHT(guess, ourGuess);
+				dismember(guess, ourGuess);
 				attempts++;
 
 			}
