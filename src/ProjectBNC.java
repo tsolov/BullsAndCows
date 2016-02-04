2a6bdd8577e4397be2f9413113135cd7f4dd42fb
jordan tsolov <jordan_tsolov@abv.bg>
2016-02-04 13:27:43 +0200

    changed the msgboxes

--- src/ProjectBNC.java
+++ src/ProjectBNC.java
@@ -95,17 +95,17 @@ public class ProjectBNC {
 	}
 
 	public static void gameInfo() {
-		System.out.println("********************************************************************");
-		System.out.println("*                                                                  *");
-		System.out.println("Bulls and Cows Game is also known as MasterMind.                   *");
-		System.out.println("Computer selects a four digit number,all four digits are different.*");
-		System.out.println("In current implementation number may not begin with 0.             *");
-		System.out.println("Cows column displays total number of digits you guessed right,     *");
-		System.out.println("bulls shows how many of those were placed at the right spots.      *");
-		System.out.println("*                                                                  *");
-		System.out.println("*           If you want to go back to the menu click 0 !           *");
-		System.out.println("*                  If you want to exit  click 1 !                  *");
-		System.out.println("********************************************************************");
+		System.out.println("**********************************************************************");
+		System.out.println("*                                                                    *");
+		System.out.println("* Bulls and Cows Game is also known as MasterMind.                   *");
+		System.out.println("* Computer selects a four digit number,all four digits are different.*");
+		System.out.println("* In current implementation number may not begin with 0.             *");
+		System.out.println("* Cows column displays total number of digits you guessed right,     *");
+		System.out.println("* bulls shows how many of those were placed at the right spots.      *");
+		System.out.println("*                                                                    *");
+		System.out.println("*           If you want to go back to the menu click 0 !             *");
+		System.out.println("*                  If you want to exit  click 1 !                    *");
+		System.out.println("**********************************************************************");
 
 		Scanner input = new Scanner(System.in);
 		int choice = input.nextInt();
@@ -131,20 +131,20 @@ public class ProjectBNC {
 	}
 
 	public static void FAQ() {
-		System.out.println("**************************************************************************************");
-		System.out.println("*                                                                                    *");
-		System.out.println("Question: Were any bulls or cows harmed in this game?                                *");
-		System.out.println("Answer: No they are all fine !!                                                      *");
-		System.out.println("Question: What will happen if i write a number bigger than 9999 or smaller than 1000?*");
-		System.out.println("Answer: You will get the option to write a new number and the last one will not work.*");
-		System.out.println("Question: What should i do to win the game ?                                         *");
-		System.out.println("Answer: You should write the exact number that the computer chose.                   *");
-		System.out.println("Question: What if i enter something different than an integer?                       *");
-		System.out.println("Answer: You are directly removed from the game and kicked to the starting menu!      *");
-		System.out.println("*                                                                                    *");
-		System.out.println("*               If you want to go back to the menu click 0 !                         *");
-		System.out.println("*                       If you want to exit  click 1 !                               *");
-		System.out.println("**************************************************************************************");
+		System.out.println("****************************************************************************************");
+		System.out.println("*                                                                                      *");
+		System.out.println("* Question: Were any bulls or cows harmed in this game?                                *");
+		System.out.println("* Answer: No they are all fine !!                                                      *");
+		System.out.println("* Question: What will happen if i write a number bigger than 9999 or smaller than 1000?*");
+		System.out.println("* Answer: You will get the option to write a new number and the last one will not work.*");
+		System.out.println("* Question: What should i do to win the game ?                                         *");
+		System.out.println("* Answer: You should write the exact number that the computer chose.                   *");
+		System.out.println("* Question: What if i enter something different than an integer?                       *");
+		System.out.println("* Answer: You are directly removed from the game and kicked to the starting menu!      *");
+		System.out.println("*                                                                                      *");
+		System.out.println("*               If you want to go back to the menu click 0 !                           *");
+		System.out.println("*                       If you want to exit  click 1 !                                 *");
+		System.out.println("****************************************************************************************");
 		Scanner input = new Scanner(System.in);
 		int choice = input.nextInt();
 		switch (choice) {
